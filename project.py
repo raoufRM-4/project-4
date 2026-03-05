07251a1ca4262403be9a0ba932526ee3830c483b
raouf <300156533@monboreal.ca>
2026-03-05 13:03:44 -0500

    t

--- project.py
+++ project.py
@@ -3,6 +3,11 @@ class Employe:
         self.nm_permis=nm_permis
         self.nom=nom
         self.prenom=prenom
-        self.voiture_service=self.voiture_service
-
+        self.voiture_service=None
+    def afficher(self):
+        print(f"le nom de la personne est:{self.nom}{self.prenom},son numero de permis est:{self.nm_permis}")
+        if self.voiture_service:
+            print("voiture attribuee")
+        else:
+            print("auucne voiture attribuee")
         
