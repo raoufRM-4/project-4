9dd33799db7bd7fd7205190722dada4cfd51b358
raouf <300156533@monboreal.ca>
2026-03-05 12:58:20 -0500

    t

---
+++ project.py
@@ -0,0 +1,8 @@
+class Employe:
+    def __init__(self,nm_permis,nom,prenom):
+        self.nm_permis=nm_permis
+        self.nom=nom
+        self.prenom=prenom
+        self.voiture_service=self.voiture_service
+
+        
